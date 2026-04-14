0488a1ce00eb977d188ccb51cf2ebdd95f2ff021
Agus Silva Sansó <silvasansoagustin@gmail.com>
2026-04-14 12:53:18 -0300

    Update maximums.py

--- maximums.py
+++ maximums.py
@@ -1,18 +1,10 @@
-# Replace the "ANSWER HERE" for your answer
-
 def max_of_two(x, y):
-    """Given x and y, that are 2 numbers, return the biggest number."""
-    if x>y:
+    if x > y:
         return x
     else:
         return y
 
 def max_of_three(x, y, z):
-    """Given x, y and z, that are 3 numbers, return the biggest number of the three."""
-
-    if y<=x and x>=z:
-        return x
-    elif y>=x and y>=z:
-        return y
-    else:
-        return z
+    # Usamos la función anterior para asegurar el resultado correcto
+    mayor_de_dos = max_of_two(x, y)
+    return max_of_two(mayor_de_dos, z)
